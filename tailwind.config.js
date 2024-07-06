76ab307c14f46538174eff45d608fcf7b01e49c1
RianDoang <muhamadrynsyy270521@gmail.com>
2024-07-07 00:25:16 +0700

    Menambah daftar game serta responsif untuk mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,9 @@ module.exports = {
   content: ["index.html", "src/script.js"],
   theme: {
     extend: {
-      colors: {
-        dark1: "#222831",
-        dark2: "#393E46",
-        dark3: "#00ADB5",
-        dark4: "#00FFF5",
-      },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
       },
     },
   },
